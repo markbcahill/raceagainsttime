415de73a50bd5b693a60b6730ade1e450d6b6417
Mark Cahill <markbaconcahill@gmail.com>
2021-03-08 15:10:29 -0600

    fixed music bug

--- state0.js
+++ state0.js
@@ -218,6 +218,7 @@ demo.state0.prototype = {
         if(Humphrey.body.y >=960){
             game.camera.unfollow();
             this.killPlayer();
+            music.stop();
         }
 
         if(playerDead == true){
